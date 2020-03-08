0088f07080e275ff294a3e330a378ae1298d7d84
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-03-08 14:54:01 -0400

    implemented error handlers

--- backend/src/api.py
+++ backend/src/api.py
@@ -85,10 +85,35 @@ def update_drink():
 def delete_drink(id_num):
     return jsonify({"success": True, "delete": 0}), 200
 
+
 ## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+                    "success": False,
+                    "error": 401,
+                    "message": "unauthorized"
+                    }), 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({
+                    "success": False,
+                    "error": 403,
+                    "message": "forbidden"
+                    }), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -96,25 +121,3 @@ def unprocessable(error):
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
